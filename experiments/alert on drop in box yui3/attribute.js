d9e2bb3f638a11e45047234a3378f6ecbb6dc27e
JD Bothma <jbothma@gmail.com>
2010-01-19 23:43:22 +0000

    functionally add root of attribute

--- experiments/alert on drop in box yui3/attribute.js
+++ experiments/alert on drop in box yui3/attribute.js
@@ -3,9 +3,20 @@ function init() {
 		filter:'min'
 	}).use('dd-drag','dd-drop','dd-proxy','node','event', function (Y) {
 
-		function newAttrBox() {
-			var attrBox = Y.Node.create('<div class="attrBox"></div>');
+		function newAttr() {
+			var attrBox = Y.Node.create('<div class="attr_box"></div>');
+			var attrBoxValue = Y.Node.create('<div class="attr_box_value">bob</div>');
+			var subLevelContainer = Y.Node.create('<div class="sub_level_container"></div>');
+			
+			attrBox.append(attrBoxValue);
+			attrBox.append(subLevelContainer);
+			
+			new Y.DD.Drag({node: attrBox});
+			
+			Y.one('body').append(attrBox);
 		}
+		
+		newAttr();
 	});
 }
 	
