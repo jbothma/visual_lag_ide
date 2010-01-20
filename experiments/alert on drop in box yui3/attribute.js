eee51695a498929efaf391ef026f43a2a95edfd8
JD Bothma <jbothma@gmail.com>
2010-01-20 00:23:11 +0000

    create and append each level functionally - next up is doing this through menus or typing

--- experiments/alert on drop in box yui3/attribute.js
+++ experiments/alert on drop in box yui3/attribute.js
@@ -3,20 +3,43 @@ function init() {
 		filter:'min'
 	}).use('dd-drag','dd-drop','dd-proxy','node','event', function (Y) {
 
-		function newAttr() {
+		function newAttrLevel(levelValue) {
 			var attrBox = Y.Node.create('<div class="attr_box"></div>');
-			var attrBoxValue = Y.Node.create('<div class="attr_box_value">bob</div>');
+			var attrBoxValue = Y.Node.create('<div class="attr_box_value">' + levelValue + '</div>');
 			var subLevelContainer = Y.Node.create('<div class="sub_level_container"></div>');
 			
 			attrBox.append(attrBoxValue);
 			attrBox.append(subLevelContainer);
 			
-			new Y.DD.Drag({node: attrBox});
+			attrBox.valueDiv = attrBoxValue;
+			attrBox.subLevelContainer = subLevelContainer;
 			
-			Y.one('body').append(attrBox);
+			attrBox.addSubLevel = function(attrLevel) {
+				attrLevel.addClass('sub_level');
+				this.subLevelContainer.append(attrLevel);
+			}
+			
+			//new Y.DD.Drag({node: attrBox});
+			
+			return attrBox;
 		}
 		
-		newAttr();
+		function addRootAttrLevel() {
+			Y.one('body').append(newAttrLevel());
+		}
+		
+		//root
+		var rootLevel = newAttrLevel('PM');
+		//sub secondary
+		var secondaryLevel = newAttrLevel('Concept');
+		rootLevel.addSubLevel(secondaryLevel);
+		//sub final
+		var finalLevel = newAttrLevel('visible');
+		secondaryLevel.addSubLevel(finalLevel);
+		//add to body
+		Y.one('body').append(rootLevel);
+		//make root tragable
+		new Y.DD.Drag({node: rootLevel});
 	});
 }
 	
