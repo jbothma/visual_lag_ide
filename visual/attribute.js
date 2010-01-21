f0e6edb2d707b85ce4cae5eaa3c3f6998da27578
JD Bothma <jbothma@gmail.com>
2010-01-21 23:03:49 +0000

    attribute creator can take isRoot and sublevel as parameter

--- visual/attribute.js
+++ visual/attribute.js
@@ -1,49 +1,53 @@
 /**
- * Library for the LAG Attribute visual item
- */ 
+* Library for the LAG Attribute visual item
+*/ 
 
-function init() {
-	YUI({
-		filter:'min'
-	}).use('dd-drag','dd-drop','dd-proxy','node','event', function (Y) {
 
-		function newAttrLevel(levelValue) {
-			var attrBox = Y.Node.create('<div class="attr_box"></div>');
-			var attrBoxValue = Y.Node.create('<div class="attr_box_value">' + levelValue + '</div>');
-			var subLevelContainer = Y.Node.create('<div class="sub_level_container"></div>');
-			
-			attrBox.append(attrBoxValue);
-			attrBox.append(subLevelContainer);
-			
-			attrBox.valueDiv = attrBoxValue;
-			attrBox.subLevelContainer = subLevelContainer;
-			
-			attrBox.addSubLevel = function(attrLevel) {
-				attrLevel.addClass('sub_level');
-				this.subLevelContainer.append(attrLevel);
-			}
-			
-			//new Y.DD.Drag({node: attrBox});
-			
-			return attrBox;
+YUI({
+	filter:'min'
+}).use('dd-drag','dd-drop','dd-proxy','node','event', function (Y) {
+
+	/**
+	 *
+	 */
+	function newAttrLevel(levelValue, isRoot, sublevel) {
+		var attrBox = Y.Node.create('<div class="attr_box"></div>');
+		var attrBoxValue = Y.Node.create('<div class="attr_box_value">' + levelValue + '</div>');
+		var subLevelContainer = Y.Node.create('<div class="sub_level_container"></div>');
+		
+		attrBox.append(attrBoxValue);
+		attrBox.append(subLevelContainer);
+		
+		attrBox.valueDiv = attrBoxValue;
+		attrBox.subLevelContainer = subLevelContainer;
+		
+		attrBox.addSubLevel = function(attrLevel) {
+			attrLevel.addClass('sub_level');
+			this.subLevelContainer.append(attrLevel);
 		}
 		
-		function addRootAttrLevel() {
-			Y.one('body').append(newAttrLevel());
+		if (isset(sublevel)) {
+			attrBox.addSubLevel(sublevel);
 		}
 		
-		//root
-		var rootLevel = newAttrLevel('PM');
-		//sub secondary
-		var secondaryLevel = newAttrLevel('Concept');
-		rootLevel.addSubLevel(secondaryLevel);
-		//sub final
-		var finalLevel = newAttrLevel('visible');
-		secondaryLevel.addSubLevel(finalLevel);
-		//add to body
-		Y.one('body').append(rootLevel);
-		//make root tragable
-		new Y.DD.Drag({node: rootLevel});
-	});
-}
+		if (isset(isRoot) && isRoot) {
+			new Y.DD.Drag({node: attrBox});
+		}
+		
+		return attrBox;
+	}
 	
+	Y.one('body').append(newAttrLevel('PM',true,newAttrLevel('Concept',false,newAttrLevel('visible'))));
+
+	function newLAGAttr(levels) {
+		rootAttrLevel = newAttrLevel(levels[0],true);
+		
+		for (var i = 1; i < levels.length; i++) {
+			
+			
+			newAttrLevel(levels[i]);
+		}
+	}
+		
+});
+
