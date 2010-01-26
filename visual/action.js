48504af48fccf1095671c78f5a4d86437cf684ac
JD Bothma <jbothma@gmail.com>
2010-01-26 00:22:01 +0000

    Add LAG Grammar Action production to visual_editor.htm
    Some small UI CSS like non-transparent Attributes
    
    Single level custom attribute for custom values in actions

--- visual/action.js
+++ visual/action.js
@@ -7,33 +7,35 @@ getMyY().use('dd-drag','dd-proxy','dd-drop','node','event', function (Y) {
 	//Listen for all drag:drophit events
 	Y.DD.DDM.on('drop:hit', function(e) {
 		if (isset(e.drag.attribute) && isset(e.drop.attributeContainer)) {
-			e.drop.attributeContainer.append(e.drag.attribute);
-			e.drag.attribute.addClass('child-of-action');
-			e.drag.attribute.setStyle('left',0);
-			e.drag.attribute.setStyle('top',0);
+			e.drop.attributeContainer.catch(e.drag.attribute);
 		}
 	});
 	
 	
 	LAGVEActn.newAction = function(targetId) {
-		var action = LAGVECon.newConnectable('workspace');
+		var action = LAGVECon.newConnectable(targetId);
 		
-		var attributeContainer	= Y.Node.create( '<div class="action-attribute-container action-container"></div>' );
-		var operatorContainer 	= Y.Node.create( '<div class="action-operator-container action-container"></div>' );
-		var valueContainer 		= Y.Node.create( '<div class="action-value-container action-container">v</div>' );
+		var attributeContainer	= Y.Node.create( '<div class="action-attribute-container action-child-container"></div>' );
+		var valueContainer		= Y.Node.create( '<div class="action-attribute-container action-child-container"></div>' );
+		var operatorContainer 	= Y.Node.create( '<div class="action-operator-container action-child-container"></div>' );
 		
-		var attributeContainerDT = new Y.DD.Drop({ node: attributeContainer });
+		var attributeContainerDT	= new Y.DD.Drop({ node: attributeContainer });
+		var valueContainerDT 		= new Y.DD.Drop({ node: valueContainer });
 		
-		attributeContainerDT.attributeContainer = attributeContainer;
+		attributeContainerDT.attributeContainer	= attributeContainer;
+		valueContainerDT.attributeContainer		= valueContainer;
 
 		/**
 		 * Given a Node, attributeContainer will receive it as a child
 		 */
-		attributeContainer.catch = function(e) {
-			
-			var x = 1;
-			//this.append(attribute);
+		attributeContainer.catch = function(attribute) {
+			this.append(attribute);
+			attribute.addClass('action-child');
+			attribute.setStyle('left',0);
+			attribute.setStyle('top',0);
 		}
+		
+		valueContainer.catch = attributeContainer.catch;
 		
 		var operatorSelect =  Y.Node.create( '<select class="operator-select">' +
 											 '<option value="=">=</option>' +
--- visual/utility.js
+++ visual/utility.js
@@ -3,14 +3,13 @@ function isset(variable)
 	return ( typeof(variable) == "undefined" || variable == null )?  false: true;
 }
 
-//var myY;
-
 function getMyY() {
 	if (typeof(myY) != "undefined" && myY != null) {
-		alert('myY was set, returning.');
+		//alert('myY was set, returning.');
 		return myY;
 	} else {
-		alert('no myY, creating and returning.');
+		//alert('no myY, creating and returning.');
+		// Create global YUI3 object
 		myY = YUI({ filter:'min' });
 		return myY;
 	}
