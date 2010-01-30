6320eb4c81af34c1dfb6729f03dc998ced422bf3
JD Bothma <jbothma@gmail.com>
2010-01-30 15:45:52 +0000

    Action is not dragable any more because it belongs in a statement li which is dragable and shouldn't be dragged out of it

--- visual/action.js
+++ visual/action.js
@@ -1,17 +1,8 @@
 LAGVEActn = new Object();
-LAGVEActn.scriptName = 'action.html';
+LAGVEActn.scriptName = 'action.js';
 
 getMyY().use('dd-drag','dd-proxy','dd-drop','node','event', function (Y) {
-	
-
-	//Listen for all drag:drophit events
-	Y.DD.DDM.on('drop:hit', function(e) {
-		if (isset(e.drag.attribute) && isset(e.drop.attributeContainer)) {
-			e.drop.attributeContainer.catch(e.drag.attribute);
-		}
-	});
-	
-	
+		
 	LAGVEActn.newAction = function(targetId) {
 		var action 				= Y.Node.create( '<div class="action"></div>' );
 		
@@ -27,18 +18,6 @@ getMyY().use('dd-drag','dd-proxy','dd-drop','node','event', function (Y) {
 		
 		attributeContainerDT.node	= attributeContainer;
 		valueContainerDT.node		= valueContainer;
-
-		/**
-		 * Given a Node, attributeContainer will receive it as a child
-		 */
-		attributeContainer.catch = function(attribute) {
-			this.append(attribute);
-			attribute.addClass('action-child');
-			attribute.setStyle('left',0);
-			attribute.setStyle('top',0);
-		}
-		
-		valueContainer.catch = attributeContainer.catch;
 		
 		var operatorSelect =  Y.Node.create( '<select class="operator-select">' +
 											 '<option value="=">=</option>' +
@@ -52,11 +31,21 @@ getMyY().use('dd-drag','dd-proxy','dd-drop','node','event', function (Y) {
 		action.append(operatorContainer);
 		action.append(valueContainer);
 		
-		var actionDD =	new Y.DD.Drag({node:action}).plug(	Y.Plugin.DDConstrained,
-															{ constrain2node: '#' + targetId});
-		actionDD.node = action;
+		/*	if action will always be inside a statement li,
+			action shouldn't be a drag becuase it will get dragged out of the li
 		
-		Y.one('#' + targetId).append(action);
+		var actionDD =	new Y.DD.Drag({node:action})
+		
+		if (isset(targetId)) {
+			actionDD.plug(	Y.Plugin.DDConstrained,
+							{ constrain2node: '#' + targetId});
+		}
+		
+		actionDD.node = action;*/
+		
+		if (isset(targetId)) {
+			Y.one('#' + targetId).append(action);
+		}
 		
 		return action;
 	}
